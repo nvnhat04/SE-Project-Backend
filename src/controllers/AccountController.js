2b414ce8ab031902fcb92dd85cf5f17bc40e320f
nvnhat04 <nvnhat04324@gmail.com>
2024-04-09 16:35:06 +0700

    add authentication user

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,29 +1,49 @@
 const responseHandler = require("../handlers/response.handler.js");
 const User = require('../models/user.model.js');
+const bcrypt = require('bcrypt');
 
 class AccountController {
     async signUp(req, res) {
         const newUser = req.body;
-        const find = await User.findOne({email: newUser.email});
-        if(!find) {
-            const user = await User.create(req.body);
-            res.send({email: user.email, _id: user._id});
-            console.log("success");
+        try {
+            const find = await User.findOne({email: newUser.email});
+            if (!find) {
+                // Hash the password before storing it
+                const hashedPassword = await bcrypt.hash(newUser.password, 10); // 10 is the saltRounds
+                newUser.password = hashedPassword;
+                const user = await User.create(newUser);
+                res.send({email: user.email, _id: user._id});
+                console.log("success");
+            } else {
+                res.send('0');
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
         }
-        else {
-            res.send('0');
+    }
+    
+    async login(req, res) {
+        const authUser = req.body;
+        try {
+            const user = await User.findOne({ email: authUser.email });
+            if (user) {
+                // Compare hashed password with the provided password
+                const match = await bcrypt.compare(authUser.password, user.password);
+                if (match) {
+                    res.send({ success: true, message: 'Login successful', email: user.email, _id: user._id });
+                } else {
+                    res.send({ success: false, message: 'Invalid credentials' });
+                }
+            } else {
+                res.send({ success: false, message: 'User not found' });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
         }
     }
 
-    async login(req, res) {
-        const authUser = req.body;
-        const check = await User.findOne(authUser, {_id: 1, email: 1});
-        if(check) {
-            res.send(check);
-        } else {
-            res.send('0');
-        }
-    }
     async getDetails(req, res) {
         try{
             const user = await User.find({username: req.params.username});
