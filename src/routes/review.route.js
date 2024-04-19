b434386a7604041865df4e37931ce3df4ce9c268
nvnhat04 <nvnhat04324@gmail.com>
2024-04-19 18:30:03 +0700

    feat: add review api

--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const reviewController = require('../controllers/ReviewController');
-router.post('/create', reviewController.createReview);
-router.get('/:id', reviewController.getDetails);
+const reviewController = require('../controllers/ReviewController.js');
+const tokenMiddleware = require('../middlewares/token.middleware.js');
+router.post('/create',tokenMiddleware.authenticateToken, reviewController.createReview);
+router.get('/:movieId', reviewController.getReviews);
 
 module.exports = router;
