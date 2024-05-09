49763074d23e38832e1474a7959be908a14242d5
thuthuy <125456003+tthuy123@users.noreply.github.com>
2024-05-09 16:08:28 +0700

    feat: test for all movie api

--- test/movie.test.js
+++ test/movie.test.js
@@ -12,7 +12,7 @@ chai.use(chaiHttp);
 
 describe('Movie', () => {
 
-describe('/GET movie discover', (done) => {
+describe('GET movie discover', (done) => {
     it('it should GET movie discover', function(done) {
         chai.request(server)
             .get('/movie')
@@ -24,7 +24,7 @@ describe('/GET movie discover', (done) => {
         });
     });
 
-    describe('/GET movie detail', (done) => {
+    describe('GET movie detail', (done) => {
         it('it should GET movie info', function(done) {
             chai.request(server)
                 .get('/movie/135')
@@ -35,4 +35,97 @@ describe('/GET movie discover', (done) => {
                 });
             });
         });
+
+    describe('GET movie search', (done) => {
+        it('It should GET movie search', function(done) {
+            chai.request(server)
+                .get('/movie/search?query=avengers')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+
+    describe('GET movie popular', (done) => {
+        it('It should GET movie popular', function(done) {
+            chai.request(server)
+                .get('/movie/popular')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+
+    describe('GET movie videos', (done) => {
+        it('It should GET movie videos', function(done) {
+            chai.request(server)
+                .get('/movie/videos/135')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('data');
+                    res.body.data.should.be.a('object');
+                    done();
+                });
+        });
+    });
+
+    describe('GET movie images', (done) => {
+        it('It should GET movie images', function(done) {
+            chai.request(server)
+                .get('/movie/images/135')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('backdrops');
+                    res.body.should.have.property('posters');
+                    res.body.backdrops.should.be.a('array');
+                    res.body.posters.should.be.a('array');
+
+                    done();
+                });
+        });
+    });
+
+    describe('GET movie credits', (done) => {
+        it('It should GET movie credits', function(done) {
+            chai.request(server)
+                .get('/movie/credits/135')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('data');
+                    res.body.data.should.be.a('object');
+                    res.body.data.should.have.property('cast');
+                    res.body.data.should.have.property('crew');
+                    res.body.data.cast.should.be.a('array');
+                    res.body.data.crew.should.be.a('array');
+                    res.body.data.cast[0].should.have.property('name').eql('Bob Dylan');
+                    res.body.data.crew[0].should.have.property('name').eql('Bob Dylan');
+                    done();
+                });
+        });
+    });
+
+    describe('GET movie similar', (done) => {
+        it('It should GET movie similar', function(done) {
+            chai.request(server)
+                .get('/movie/similar/135')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('data');
+                    res.body.data.should.be.a('object');
+                    res.body.data.should.have.property('results');
+                    res.body.data.results.should.be.a('array');
+                    res.body.data.results[1].should.have.property('title').eql('NAMI TAMAKI Best CONCERT "My Graduation"');
+                    done();
+                });
+        });
+    });
+
     });
